dd5864d25b3109dbc0b51ad94facc262905d4a2e
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-10-21 19:01:10 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,18 @@ client.user.setPresence({ activity: { name: activity.text, type: activity.type }
 	   .then(console.log)
   .catch(console.error);
 });
+client.on('guildMemberAdd', member => {
+	
+	const channel = member.guild.channels.cache.find(channel => channel.name === "diana-bot-test");
+	if(!channel) return;
+	
+	var embed = new Discord.MessageEmbed()
+	.setTitle(`Welcome to ${member.guild}.`)
+	.setColor(3447003)
+	.setThumbnail(member.user.displayAvatarURL())
+	.setDescription(`Hello, ${member.user}.\n\nMake sure to go to <#768439249137303573> to introduce yourself and go to <#768440695118626857> for roles of yourself.`)
+	channel.send({embed})
+});
 client.on('message', async message => {
   
 if (message.content.toLowerCase().startsWith("Hey Diana")) {
