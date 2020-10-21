5a28b0ca37c74dabf11373d45625cfd9f53c95c2
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-10-21 18:12:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-  console.log("I am ready!");
+  console.log("I am ready");
 });
 
 client.on("message", (message) => {
   if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+    message.channel.send("pong.");
   }
 });
 
