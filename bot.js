bc6d219ea6cb078d6a8533a12d2e567d4237d026
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-10-21 18:48:44 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,23 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const { prefix } = require('./config.json');
+const activities = require('./jsons/activity');
 
 client.on("ready", () => {
-  console.log("I am ready");
+  console.log("I am ready.");
+  
+  const activity = activities[Math.floor(Math.random() * activities.length)];
+client.user.setPresence({ activity: { name: activity.text, type: activity.type }, status: 'idle' })
+	   .then(console.log)
+  .catch(console.error);
 });
-
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong.");
+client.on('message', async message => {
+  
+if (message.content.toLowerCase().startsWith("Hey Diana")) {
+    message.channel.send("Hello");
+        }
+  	      });
+   
   }
 });
 
