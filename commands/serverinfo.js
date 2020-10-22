570dc4a6e2439eb340309869b0fa866c765fc4e3
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-10-21 20:03:59 -0400

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+  name: 'serverinfo',
+  description: "Gives information about the server you are in.",
+  execute(message, args){
+    message.channel.send("Pong.");
+    
+  }
+}
