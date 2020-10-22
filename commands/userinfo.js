07f0475189ac80ab4e47afc68a9b0d4fa44f8ee5
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-10-21 21:26:10 -0400

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -12,7 +12,6 @@ module.exports = {
 				  .addField("ID", `${user.id}`)
 				  .addField("Status", `${user.presence.status}`)
 				  .addField("In Server", `${message.guild.name}`)
-				  .addField("Roles", member.roles.map(r => `${r}`).join(' | '))
 				  .setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
 				  message.channel.send({embed})
   }
