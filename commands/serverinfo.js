452548fc72cc03052d7d07254fe72c5dcd10f2d0
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-10-21 20:04:39 -0400

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -2,7 +2,17 @@ module.exports = {
   name: 'serverinfo',
   description: "Gives information about the server you are in.",
   execute(message, args){
-    message.channel.send("Pong.");
+    var embed = new Discord.MessageEmbed()
+			  .setColor(3447003)
+		    .setTitle("Server Information")
+			  .setDescription(`${message.guild}'s information`)
+			  .setThumbnail(message.guild.iconURL())
+	      .addField("Server ID", message.guild.id)
+			  .addField("Owner", message.guild.owner.user.tag)
+			  .addField("Member Count", `This server has ${message.guild.memberCount} members.`)
+			  .addField("Roles Count", `This server has ${message.guild.roles.cache.size} roles.`)
+			  .addField("Emojis Count", `This Server has ${message.guild.emojis.cache.size} emojis.`)
+				 message.channel.send({embed})
     
   }
 }
