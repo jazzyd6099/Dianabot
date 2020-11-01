6cfba7ba0caecd12c59d9722194a1c42c087397f
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-11-01 12:40:14 -0500

    Update tod.js

--- commands/tod.js
+++ commands/tod.js
@@ -5,11 +5,11 @@ module.exports = {
   description: "Truth or Dare.",
   execute(message, args){
     var embed = new Discord.MessageEmbed()
-					.setColor(3447003)
-					.setTitle('Truth or Dare')
-					.setDescription('Decide on if you want to do a truth or dare. \n\n💬 : `Truth` \n\n🗨️ : `Dare`')
-					message.channel.send({embed})
-          embed.react('💬')
+		.setColor(3447003)
+		.setTitle('Truth or Dare')
+		.setDescription('Decide on if you want to do a truth or dare. \n\n💬 : `Truth` \n\n🗨️ : `Dare`')
+    		let messageEmbed = await message.channel.send({embed})
+          	messageEmbed.react('💬')
     
   }
 }
