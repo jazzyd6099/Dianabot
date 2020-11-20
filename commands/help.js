f5ba1a99b8b4c895c06715c4608670ef8deca6c8
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-11-19 23:27:19 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,1 +1,18 @@
-
+  
+module.exports = {
+  name: 'help',
+  description: "To help and tell you what Diana's commands are.",
+  execute(message, args){
+   var embed = new Discord.MessageEmbed()
+					.setTitle("My Commands")
+					.setColor(3447003)
+					.setDescription("There will be more to come. If you have any suggestions, please put them in <#768471194424442891>.")
+					.setThumbnail('https://ibb.co/WzbRqhH')
+					.addField('__Fun__', '`d/tod`, `d/8ball`, `d/coinflip`, `d/rate`')
+					.addField('__Information__', '`d/serverinfo`,`d/userinfo`')
+					.addField('__Other__', '`WIP.`')
+					.setFooter('Bot coded and created by SpaceCarame#6433.')
+					message.channel.send({embed})
+    
+  }
+}
