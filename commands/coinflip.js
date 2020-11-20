9edd8264886455130712b44871c03e866357ff06
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-11-20 00:12:13 -0500

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -17,7 +17,8 @@ module.exports = {
 	  const coinflip = ['Heads.','Tails.'];
     var embed = new Discord.MessageEmbed()
 	      .setColor(3447003)
-	      .setTitle("Coinflip")
+	      .setTitle("Coinflip. What will it land on?")
+    	      .setThumbnail('https://i.ibb.co/0BsCZnB/free-heads-or-tails-icon-456-thumb.png')
 	      .setDescription(coinflip[Math.floor(Math.random () * coinflip.length)])
     	      message.channel.send({embed})
     
